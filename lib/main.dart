ba72bb1deef6dc7154725ce848ddd5af863bf530
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2023-05-25 01:06:51 +0530

    Added refrence to Ui screen in main

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app_screen/screens/whats_in_kitchen_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyWidget(),
+    );
+  }
+}
